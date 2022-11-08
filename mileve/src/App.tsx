939a268b44634de0c05c784a24f4d042ba2732f4
Rafael Mih <rafaelwsantos@gmail.com>
2022-11-08 04:33:16 -0300

    layout figma

---
+++ mileve/src/App.tsx
@@ -0,0 +1,60 @@
+import {
+  Routes,
+  Route,
+  useNavigationType,
+  useLocation,
+} from "react-router-dom";
+import Institucional from "./pages/Institucional";
+import Institucional1 from "./pages/Institucional1";
+import { useEffect } from "react";
+
+function App() {
+  const action = useNavigationType();
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  useEffect(() => {
+    if (action !== "POP") {
+      window.scrollTo(0, 0);
+    }
+  }, [action]);
+
+  useEffect(() => {
+    let title = "";
+    let metaDescription = "";
+
+    //TODO: Update meta titles and descriptions below
+    switch (pathname) {
+      case "/":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/institucional":
+        title = "";
+        metaDescription = "";
+        break;
+    }
+
+    if (title) {
+      document.title = title;
+    }
+
+    if (metaDescription) {
+      const metaDescriptionTag: HTMLMetaElement | null = document.querySelector(
+        'head > meta[name="description"]'
+      );
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Institucional />} />
+
+      <Route path="/institucional" element={<Institucional1 />} />
+    </Routes>
+  );
+}
+export default App;
---
+++ mileve/src/pages/Institucional1.tsx
@@ -0,0 +1,211 @@
+import { FunctionComponent } from "react";
+import styles from "./Institucional1.module.css";
+
+const Institucional1: FunctionComponent = () => {
+  return (
+    <div className={styles.institucionalDiv}>
+      <div className={styles.frameDiv}>
+        <div className={styles.frameDiv1}>
+          <div className={styles.homeDiv}>Home</div>
+          <img className={styles.vectorIcon} alt="" src="../vector-1.svg" />
+          <div className={styles.frameDiv2}>
+            <div className={styles.sobreDiv}>Sobre</div>
+          </div>
+          <div className={styles.frameDiv3}>
+            <div className={styles.sobreDiv}>Soluções</div>
+            <img
+              className={styles.solidInterfaceCaretDownIcon}
+              alt=""
+              src="../solidinterfacecaret-down.svg"
+            />
+          </div>
+          <div className={styles.frameDiv4}>
+            <div className={styles.sobreDiv}>Contato</div>
+          </div>
+          <div className={styles.mobilidadeUrbanaParaTodo}>
+            <p className={styles.mobilidadeUrbana}>
+              <span>{`Mobilidade urbana `}</span>
+            </p>
+            <p className={styles.paraTodos}>
+              <span>é para</span>
+              <span className={styles.span}>{` `}</span>
+              <span className={styles.todosSpan}>todos</span>
+            </p>
+          </div>
+          <div className={styles.fundadaEm2022AMileveUm}>
+            Fundada em 2022, a Mileve é uma empresa de transportes fretados para
+            o público PCD. Temos uma frota de Ônibus, vans e carros que buscam e
+            deixam as pessoas em seu lar quando for necessário ir até a empresa
+            que é nossa cliente. Nossos clientes são empresas que se preocupam
+            com seus funcionários e querem garantir o transporte acessível para
+            todos.
+          </div>
+          <img
+            className={styles.rectangleIcon}
+            alt=""
+            src="../rectangle-13@2x.png"
+          />
+        </div>
+        <div className={styles.frameDiv5}>
+          <div className={styles.frameDiv6}>
+            <div className={styles.missoDiv}>Missão</div>
+            <div className={styles.garantirAAcessibilidadeEFa}>
+              <p className={styles.mobilidadeUrbana}>
+                Garantir a acessibilidade e facilitar a mobilidade no mercado de
+                trabalho, melhorando a qualidade de vida
+              </p>
+              <p className={styles.paraTodos}>
+                das pessoas através do nosso serviço de transporte para os
+                complexos empresariais, hospitais e empresas.
+              </p>
+            </div>
+            <div className={styles.frameDiv7}>
+              <img
+                className={styles.solidInterfaceCaretDownIcon}
+                alt=""
+                src="../solidcommunicationphone2.svg"
+              />
+              <div className={styles.contateNosDiv}>Contate-nos</div>
+            </div>
+            <div className={styles.frameDiv8}>
+              <div className={styles.contateNosDiv}>Explore nossos planos</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.frameDiv9}>
+          <img
+            className={styles.unsplashKqOLr8OiQLUIcon}
+            alt=""
+            src="../unsplashkqolr8oiqlu1@2x.png"
+          />
+          <div className={styles.fraseInspiradoraDiv}>Frase inspiradora</div>
+          <div className={styles.umaVerdadeiraInclusoProv}>
+            “Uma verdadeira inclusão é provada em ações que realmente abracem a
+            diversidade.”
+          </div>
+        </div>
+        <div className={styles.frameDiv10}>
+          <div className={styles.tecnologiaDiv}>Tecnologia</div>
+          <div className={styles.rectangleDiv} />
+          <div className={styles.rectangleDiv1} />
+          <div className={styles.clculoDeRotas}>Cálculo de rotas</div>
+          <div className={styles.dadosDemogrficosDiv}>Dados demográficos</div>
+          <div className={styles.frameDiv11}>
+            <div className={styles.rectangleDiv2} />
+            <div className={styles.rectangleDiv3} />
+            <div className={styles.paraLibrasDiv}>Para Libras</div>
+            <div className={styles.profissionaisTreinadosDiv}>
+              Profissionais Treinados
+            </div>
+          </div>
+          <div className={styles.frameDiv12}>
+            <div className={styles.rectangleDiv2} />
+            <div className={styles.rectangleDiv3} />
+            <div className={styles.paraLibrasDiv}>
+              Orimização e economia de combustível e tempo
+            </div>
+            <div className={styles.profissionaisTreinadosDiv}>
+              Rota otimizadas
+            </div>
+          </div>
+        </div>
+        <div className={styles.frameDiv13}>
+          <div className={styles.nossosPlanosESolues}>
+            Nossos planos e soluções
+          </div>
+          <img className={styles.image5Icon} alt="" src="../image-5@2x.png" />
+          <div className={styles.frameDiv14}>
+            <div className={styles.onibusFretadosParaComplexos}>
+              Onibus fretados para complexos empresariais
+            </div>
+            <div className={styles.frameDiv15}>
+              <div className={styles.contateNosDiv}>Planos disponíveis</div>
+            </div>
+          </div>
+          <div className={styles.frameDiv16}>
+            <div className={styles.onibusFretadosParaComplexos}>
+              Carros fretados para empresas
+            </div>
+            <div className={styles.frameDiv15}>
+              <div className={styles.contateNosDiv}>Planos disponíveis</div>
+            </div>
+          </div>
+          <div className={styles.frameDiv18}>
+            <div className={styles.onibusFretadosParaComplexos}>
+              Vans fretadas para hospitais
+            </div>
+            <div className={styles.frameDiv15}>
+              <div className={styles.contateNosDiv}>Planos disponíveis</div>
+            </div>
+          </div>
+          <img className={styles.image6Icon} alt="" src="../image-6@2x.png" />
+          <img className={styles.image7Icon} alt="" src="../image-7@2x.png" />
+        </div>
+        <div className={styles.frameDiv20}>
+          <div className={styles.frameDiv21}>
+            <div className={styles.frameDiv22}>
+              <img
+                className={styles.solidBrandsGmailIcon}
+                alt=""
+                src="../solidbrandsgmail.svg"
+              />
+              <div className={styles.frameDiv23}>
+                <div className={styles.emailDiv}>email</div>
+                <div className={styles.omahindevgmailcomDiv}>
+                  omahindev@gmail.com
+                </div>
+              </div>
+            </div>
+            <div className={styles.frameDiv24}>
+              <div className={styles.contateNosDiv}>Email Now</div>
+            </div>
+          </div>
+          <div className={styles.contatoDiv1}>Contato</div>
+          <div className={styles.rectangleDiv6} />
+          <div className={styles.frameDiv25}>
+            <img
+              className={styles.solidInterfaceCaretDownIcon}
+              alt=""
+              src="../solidbrandsinstagram1.svg"
+            />
+            <img
+              className={styles.solidInterfaceCaretDownIcon}
+              alt=""
+              src="../solidbrandsfacebook.svg"
+            />
+            <img
+              className={styles.solidInterfaceCaretDownIcon}
+              alt=""
+              src="../solidbrandstumblr.svg"
+            />
+            <img
+              className={styles.solidInterfaceCaretDownIcon}
+              alt=""
+              src="../solidbrandslinkedin.svg"
+            />
+          </div>
+          <div className={styles.frameDiv26}>
+            <div className={styles.frameDiv27}>
+              <img
+                className={styles.solidBrandsGmailIcon}
+                alt=""
+                src="../solidcommunicationphone3.svg"
+              />
+              <div className={styles.frameDiv28}>
+                <div className={styles.callDiv}>Call</div>
+                <div className={styles.omahindevgmailcomDiv}>
+                  +6281232936733
+                </div>
+              </div>
+            </div>
+            <div className={styles.frameDiv29}>
+              <div className={styles.contateNosDiv}>Call Now</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Institucional1;
